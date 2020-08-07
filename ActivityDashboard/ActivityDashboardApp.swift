61e1016a38916ca2e1a10622882789294e8eb686
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-07 14:34:35 +0300

    Initial Commit

---
+++ ActivityDashboard/ActivityDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityDashboardApp.swift
+//  ActivityDashboard
+//
+//  Created by ramil on 07.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct ActivityDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
